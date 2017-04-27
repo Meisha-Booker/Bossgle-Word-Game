478d23af6447fcd03603627951f0f1024e208bce
Alexandra Waite <Alexandra@MBP.local>
2017-04-27 14:27:25 -0700

    Fixed submit button

--- bossggle.js
+++ bossggle.js
@@ -1,57 +1,54 @@
 $(document).ready(function(){
-
+    var alwaysArray = []
+    var letter;
+    var finalWord;
+    var wordScore;
   $(".flex-box").each(function(index, box){
     $(box).text(addLetter())
   })
-    var alwaysArray= []
+
   $(".flex-box").click(function(){
     $(this).css("background","orange");
 
     // Grab one letter and push to array.
-    var letter = $(this).text()
+    letter = $(this).text()
     word.push(letter);
 
     // Join all the letters.
-    var finalWord= word.join("");
+    finalWord = word.join("");
 
     // This is the final word score.
     // Each letter is 9 points.
-    var wordScore= (finalWord.length * 9)
+    wordScore = (finalWord.length * 9)
     console.log(wordScore);
 
-    // var newArray = []
-    // newArray.push([finalWord, wordScore])
     alwaysArray.push(finalWord)
     console.log(finalWord);
 
     $('.unfinished-word').text(finalWord);
+  })
 
     $(".sub-button").click(function() {
       $(".flex-box").css("background","white");
 
-      $('.words').text(finalWord + " " + wordScore);
+      var newWord = document.createElement('p')
+      newWord.innerText = finalWord + " " + wordScore
+      document.querySelector('.words').appendChild(newWord);
 
       $('.score-right').text(wordScore);
       word = []
       console.log(alwaysArray);
-
       $('.unfinished-word').text();
-        // var completedWord = $('.unfinished-word').text(word.join(""));
-        // $(".sub-button").click(function() {
-        //   $('.words').replace(completedWord);
     })
 
-    // $(".sub-button").click(function() {
-    //   $('.words').text(finalWord);
-
-  })
-
   $(".clear-button").click(function() {
+    $(".flex-box").css("background","white");
     $('.unfinished-word').remove();
   })
 
   $(".reset-button").click(function() {
-    $('.unfinished-word').remove();
+    $(".flex-box").css("background","white");
+    $('.unfinished-word').detach();
     $(".flex-box").each(function(index, box) {
       $(box).text(addLetter())
     })
@@ -67,8 +64,4 @@ var letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 function addLetter() {
   var index= Math.floor(Math.random() * letters.length) +1;
   return letters[index -1];
-}
-
-function scoreCalculator() {
-
 }
